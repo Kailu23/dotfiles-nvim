42117a620bb8b0321c3c7422e56a2fd43ac8d61a
Marin Valenčak <valencakmarin@gmail.com>
2026-04-09 00:31:08 +0200

    fix(config): roslyn.nvim settings weren't applied
        - load roslyn when starting editor
        - add markdown_oxide lsp for *.md files

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,7 +1,7 @@
 require("nvchad.configs.lspconfig").defaults()
 
 -- local servers = { "html", "cssls", "lua-language-server", "emmet-ls" }
-local servers = { "cssls", "lua-language-server", "emmet_language_server", "typescript-language-server"}
+local servers = { "cssls", "lua-language-server", "emmet_language_server", "typescript-language-server", "markdown_oxide"}
 vim.lsp.config("emmet", {})
 vim.lsp.config("typescript-language-server", {
     cmd = {"typescript-language-server", "--stdio"},
@@ -10,25 +10,6 @@ vim.lsp.config("typescript-language-server", {
 vim.lsp.enable(servers)
 
 -- read :h vim.lsp.config for changing options of lsp servers
-vim.lsp.config("roslyn", {
-  settings = {
-    ["csharp|inlay_hints"] = {
-      csharp_enable_inlay_hints_for_implicit_object_creation = true,
-      csharp_enable_inlay_hints_for_implicit_variable_types = true,
-      csharp_enable_inlay_hints_for_lambda_parameter_types = true,
-      csharp_enable_inlay_hints_for_types = true,
-      dotnet_enable_inlay_hints_for_indexer_parameters = true,
-      dotnet_enable_inlay_hints_for_literal_parameters = true,
-      dotnet_enable_inlay_hints_for_object_creation_parameters = true,
-      dotnet_enable_inlay_hints_for_other_parameters = true,
-      dotnet_enable_inlay_hints_for_parameters = true,
-      dotnet_suppress_inlay_hints_for_parameters_that_differ_only_by_suffix = true,
-      dotnet_suppress_inlay_hints_for_parameters_that_match_argument_name = true,
-      dotnet_suppress_inlay_hints_for_parameters_that_match_method_intent = true,
-    },
-  },
-})
-
 vim.diagnostic.config {
   underline = false,
   virtual_text = false,
--- lua/plugins/roslyn.lua
+++ lua/plugins/roslyn.lua
@@ -1,8 +1,12 @@
 return {
   "seblyng/roslyn.nvim",
+  lazy = false,
   ---@module 'roslyn.config'
   ---@type RoslynNvimConfig
   ft = { "cs", "razor" },
+  config = function ()
+    require("configs.lsp.roslyn")
+  end,
   opts = {
     -- your configuration comes here; leave empty for default settings
   },
