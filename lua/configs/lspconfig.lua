93fb2acf4724640c4d8ba5f3acac2e4b45fa552b
Marin Valenčak <valencakmarin@gmail.com>
2026-04-13 17:36:47 +0200

    refactor(deprecated): remove function vim.lsp.with()

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -24,7 +24,3 @@ vim.diagnostic.config {
     },
   },
 }
-
-vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
-  virtual_text = false,
-})
