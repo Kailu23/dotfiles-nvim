a7da6f7e369ba0089e46516d0f3d19438275e4e9
Marin Valenčak <valencakmarin@gmail.com>
2026-05-24 23:46:35 +0200

    feat(nvim): add yank highlighting

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -1,1 +1,10 @@
 require "nvchad.autocmds"
+
+vim.api.nvim_create_autocmd('TextYankPost', {
+    group = vim.api.nvim_create_augroup('highlight_yank', {}),
+    desc = "Higlight selection on yank",
+    pattern = "*",
+    callback = function ()
+        vim.highlight.on_yank { higroup = 'IncSearch', timeout = 100}
+    end,
+})
