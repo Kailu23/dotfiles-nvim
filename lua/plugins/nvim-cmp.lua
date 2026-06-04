dace345fd508393a15bff89ff22dee5f9d83e82c
Marin Valenčak <valencakmarin@gmail.com>
2026-06-04 15:19:58 +0200

    fix(nvim-cmp): scrollbar wasn't appearing
        - scrollbar wasn't appearing correctly because I overwrote NvChad's
          default config

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -1,8 +1,11 @@
 return {
   "hrsh7th/nvim-cmp",
   opts = function(_, opts)
-      local cmp = require "cmp"
-    opts = require "configs.nvim-cmp-config"
-    cmp.setup(opts)
+    local cmp = require "cmp"
+    local config = require "configs.nvim-cmp-config"
+
+    opts = vim.tbl_deep_extend('force', opts, config)
+
+    return opts
   end,
 }
