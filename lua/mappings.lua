5cb347508abb54c30900d88022247dd8f344ceb1
Marin Valenčak <valencakmarin@gmail.com>
2026-07-04 17:24:27 +0200

    chore(mappings): fix whitespace

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -8,15 +8,15 @@ local map = vim.keymap.set
 require("custom-mappings")
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
-map("i", "jk", "<ESC>", { desc = "enter normal mode"})
-map("i", "kj", "<ESC>", { desc = "enter normal mode"})
-map("n", "<leader>ot", "<CMD>terminal<CR>", { desc = "open terminal"})
+map("i", "jk", "<ESC>", { desc = "enter normal mode" })
+map("i", "kj", "<ESC>", { desc = "enter normal mode" })
+map("n", "<leader>ot", "<CMD>terminal<CR>", { desc = "open terminal" })
 
-map("t", "<ESC>", "<C-\\><C-n>", { desc = "enter normal mode"})
+map("t", "<ESC>", "<C-\\><C-n>", { desc = "enter normal mode" })
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
 
-map("n", "<leader>qo", "<cmd>copen<CR>", vim.tbl_extend('force', opts, { desc = "Open quickfix window"}) )
-map("n", "<leader>qc", "<cmd>cclose<CR>", vim.tbl_extend('force', opts, { desc = "Close quickfix window"}))
+map("n", "<leader>qo", "<cmd>copen<CR>", vim.tbl_extend("force", opts, { desc = "Open quickfix window" }))
+map("n", "<leader>qc", "<cmd>cclose<CR>", vim.tbl_extend("force", opts, { desc = "Close quickfix window" }))
 
-map("v", "K", ":m '<-2<CR>gv=gv", vim.tbl_extend('force', opts, { desc = "Move highlighted line/lines up"}))
-map("v", "J", ":m '>+1<CR>gv=gv", vim.tbl_extend('force', opts, { desc = "Move highlighted line/lines up"}))
+map("v", "K", ":m '<-2<CR>gv=gv", vim.tbl_extend("force", opts, { desc = "Move highlighted line/lines up" }))
+map("v", "J", ":m '>+1<CR>gv=gv", vim.tbl_extend("force", opts, { desc = "Move highlighted line/lines up" }))
