68e2131c8e6c14d595a0435cffa9dea5a9f2d265
Marin Valenčak <valencakmarin@gmail.com>
2026-08-08 22:30:51 +0200

    chore(mappings): remove unnecessary bind

--- lua/core/mappings.lua
+++ lua/core/mappings.lua
@@ -10,7 +10,6 @@ require("core.mappings.init")
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>", { desc = "enter normal mode" })
 map("i", "kj", "<ESC>", { desc = "enter normal mode" })
-map("n", "<leader>ot", "<CMD>terminal<CR>", { desc = "open terminal" })
 
 map("t", "<ESC>", "<C-\\><C-n>", { desc = "enter normal mode" })
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
