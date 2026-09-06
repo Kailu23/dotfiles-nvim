efbbff2640f24bdadde7a069adaf72858a1d9fb3
Marin Valenčak <valencakmarin@gmail.com>
2026-09-06 15:19:54 +0200

    refactor(autocmds): replace deprecated API

--- lua/core/autocmds.lua
+++ lua/core/autocmds.lua
@@ -5,6 +5,6 @@ vim.api.nvim_create_autocmd('TextYankPost', {
     desc = "Higlight selection on yank",
     pattern = "*",
     callback = function ()
-        vim.hl.on_yank { higroup = 'IncSearch', timeout = 100}
+        vim.hl.hl_op({higroup = 'IncSearch', timeout = 100})
     end,
 })
